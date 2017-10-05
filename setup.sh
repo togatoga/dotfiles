c141bfde3f56a87caf61c285786d1c7141ae7999
togatoga <togasakitogatoga@gmail.com>
2017-10-06 06:28:15 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ];then
+    ./homebrew/install.sh
+fi
+
+# go
+echo "Set up Golang..."
+./go/get.sh
+
+# rust
+echo "Set up Rust...."
+if test ! $(which rustc);then
+    echo "Install Rust..."
+    ./rust/install.sh
+fi
+./rust/get.sh
+
+

46fa945ed030044d6d2ef70ffdac2f9b685e6367
togatoga <togasakitogatoga@gmail.com>
2017-10-05 23:48:19 +0900

    Update .peco

--- .peco/peco-cdr.zsh
+++ .peco/peco-cdr.zsh
@@ -1,6 +1,6 @@
 # Change directory where I visited before
 function peco-cdr () {
-    local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
+    local selected_dir=$(cdr -l | awk '{ print $2 }' | peco --prompt '[cdr]')
     if [ -n "$selected_dir" ]; then
         BUFFER="cd ${selected_dir}"
         zle accept-line

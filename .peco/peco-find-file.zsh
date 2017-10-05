a0ad9720739849b60952e4cb208a9f812dc41ac8
togatoga <togasakitogatoga@gmail.com>
2017-10-05 09:16:28 +0900

    Fix indent

--- .peco/peco-find-file.zsh
+++ .peco/peco-find-file.zsh
@@ -4,17 +4,17 @@ function peco-find-file() {
 		source_files=$(git ls-files)
 	else
 		source_files=$(find . -type f)
-			fi
-			selected_files=$(echo $source_files | peco --prompt "[find file]")
+	fi
+	selected_files=$(echo $source_files | peco --prompt "[find file]")
 
-			result=''
-			for file in $selected_files; do
-				result="${result}$(echo $file | tr '\n' ' ')"
-					done
+	result=''
+	for file in $selected_files; do
+		result="${result}$(echo $file | tr '\n' ' ')"
+	done
 
-					BUFFER="${BUFFER}${result}"
-					CURSOR=$#BUFFER
-					zle redisplay
+	BUFFER="${BUFFER}${result}"
+	CURSOR=$#BUFFER
+	zle redisplay
 }
 zle -N peco-find-file
 bindkey '^s' peco-find-file

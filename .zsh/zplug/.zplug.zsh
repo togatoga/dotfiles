d07ee7439da6fc3c0905e3f0c9113766dde486dd
togatoga <togasakitogatoga@gmail.com>
2017-10-06 22:54:40 +0900

    Update .zplug.zsh

--- .zsh/zplug/.zplug.zsh
+++ .zsh/zplug/.zplug.zsh
@@ -5,13 +5,20 @@ source ~/.zplug/init.zsh
 zplug "chrissicool/zsh-256color", defer:2
 zplug "Tarrasch/zsh-colors", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "ascii-soup/zsh-url-highlighter", defer:2
+zplug "ascii-soup/zsh-url-highlighter", defer:3
 
 # completions
 zplug "zsh-users/zsh-completions", defer:2
 
-#emoji
-zplug "b4b4r07/emoji-cli", as:command
+# bookmark
+zplug "jocelynmallon/zshmarks", defer:1
+
+# theme
+#zplug mafredri/zsh-async, from:github
+#zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
+
+# autoenv
+zplug "Tarrasch/zsh-autoenv"
 
 # 未インストール項目をインストールする
 if ! zplug check --verbose; then

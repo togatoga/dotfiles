58eeeb6c2de712d0c5af841ebd09077a9e491a66
togatoga <togasakitogatoga@gmail.com>
2017-10-06 23:24:09 +0900

    Add peco-z-search.zsh

---
+++ .peco/peco-z-search.zsh
@@ -0,0 +1,18 @@
+function peco-z-search
+{
+  which peco z > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Please install peco and z"
+    return 1
+  fi
+  local res=$(z | sort -rn | cut -c 12- | peco --prompt "[dir]")
+  if [ -n "$res" ]; then
+    BUFFER+="cd $res"
+    zle accept-line
+  else
+    return 1
+  fi
+}
+zle -N peco-z-search
+bindkey '^p' peco-z-search
+

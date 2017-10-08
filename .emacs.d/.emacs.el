dbf329cde7d613f147d16c9515639e043303e3f8
togatoga <togasakitogatoga@gmail.com>
2017-10-08 17:35:20 +0900

    Update .emacs.el

--- .emacs.d/.emacs.el
+++ .emacs.d/.emacs.el
@@ -1,52 +1,5 @@
-e t)
+;; Package initialize
+(package-initialize)
 
-;; 行数を表示する
+;; Show Line
 (global-linum-mode t)
-
-;; カーソルの点滅をやめる
-(blink-cursor-mode 0)
-
-;; カーソル行をハイライトする
-(global-hl-line-mode t)
-
-;; 対応する括弧を光らせる
-(show-paren-mode 1)
-
-;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
-(setq show-paren-style 'mixed)
-(set-face-background 'show-paren-match-face "grey")
-(set-face-foreground 'show-paren-match-face "black")
-
-;; スペース、タブなどを可視化する
-(global-whitespace-mode 1)
-
-;; スクロールは１行ごとに
-(setq scroll-conservatively 1)
-
-;; シフト＋矢印で範囲選択
-(setq pc-select-selection-keys-only t)
-(pc-selection-mode 1)
-
-;; C-kで行全体を削除する
-(setq kill-whole-line t)
-
-;;; dired設定
-(require 'dired-x)
-
-;; "yes or no" の選択を "y or n" にする
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; beep音を消す
-(defun my-bell-function ()
-  (unless (memq this-command
-		'(isearch-abort abort-recursive-edit exit-minibuffer
-				keyboard-quit mwheel-scroll down up next-line previous-line
-				backward-char forward-char))
-    (ding)))
-(setq ring-bell-function 'my-bell-function)
-
-;; Macのキーバインドを使う
-(mac-key-mode 1)
-
-;; Macのoptionをメタキーにする
-(setq mac-option-modifier 'meta)

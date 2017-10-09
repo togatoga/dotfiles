a6e03eb21fe559ac1160e36403619745ec538ead
togatoga <togasakitogatoga@gmail.com>
2017-10-10 06:31:45 +0900

    Japanese .emacs.el

--- .emacs.d/.emacs.el
+++ .emacs.d/.emacs.el
@@ -1,3 +1,12 @@
+;; Japanese
+(set-language-environment 'Japanese)    ; 日本語環境
+(set-default-coding-systems 'utf-8-unix)    ; UTF-8 が基本
+(set-terminal-coding-system 'utf-8-unix)    ; emacs -nw も文字化けしない
+(setq default-file-name-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+(prefer-coding-system 'utf-8-unix)
+
+
 ;; Package initialize
 (package-initialize)
 

2471623c4dbb7191b689a396c90f713d838d88f0
togatoga <togasakitogatoga@gmail.com>
2017-10-10 06:25:33 +0900

    Update .emacs.el

--- .emacs.d/.emacs.el
+++ .emacs.d/.emacs.el
@@ -3,3 +3,20 @@
 
 ;; Show Line
 (global-linum-mode t)
+
+;; diable show startup-message
+(setq inhibit-startup-message t)
+
+;; Color
+(global-font-lock-mode t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; highlight paren
+(show-paren-mode t)
+
+;; highlight selected area
+(transient-mark-mode t)
+
+;; disable beep
+(setq ring-bell-function 'ignore)

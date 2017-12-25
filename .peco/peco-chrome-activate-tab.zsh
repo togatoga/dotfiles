b8d04c649e2b20f551d452406c628f6a94eeb36e
togatoga <togasakitogatoga@gmail.com>
2017-12-25 10:46:59 +0900

    Update .peco/peco-chrome-activate-tab.zsh

--- .peco/peco-chrome-activate-tab.zsh
+++ .peco/peco-chrome-activate-tab.zsh
@@ -4,8 +4,19 @@ function peco-chrome-activate-tab() {
         echo "Please install chrome-cli"
         return 1
     fi
-    local tab_id=$(chrome-cli list tabs | peco --prompt "[tab]" | grep -oE '\[[0-9]*\]' | tr -d '\[\]')
-    chrome-cli activate -t ${tab_id}
+    local id=$(chrome-cli list tabs | peco --prompt "[tab]" | grep -oE '\[[0-9:]*\]' | tr -d '\[\]')
+    id=(${(s/:/)id})
+    size=${#id[@]}
+    if [ ${size} = '1' ];then
+        local tab_id=${id[1]}
+        chrome-cli activate -t ${tab_id}
+    elif [ ${size} = '2' ];then
+        local tab_id=${id[2]}
+        chrome-cli activate -t ${tab_id}
+    fi
+    # if [ ! -z ${tab_id} ];then
+    #     chrome-cli activate -t ${tab_id}
+    # fi
 }
 zle -N peco-chrome-activate-tab
 bindkey '^\' peco-chrome-activate-tab

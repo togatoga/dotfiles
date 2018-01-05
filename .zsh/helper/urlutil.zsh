51907ec5e4baf8b7ca829a27cc4ae840c98fdbc6
togatoga <togasakitogatoga@gmail.com>
2018-01-05 16:18:36 +0900

    Add urlutil.zsh

---
+++ .zsh/helper/urlutil.zsh
@@ -0,0 +1,26 @@
+
+# Encode a URL
+function urlencode() {
+
+    local url=$1
+
+    if [[ $url == "" ]]; then
+        echo "A string is needed to encode"
+        return
+    fi
+
+    python -c "import urllib.parse as u; print(u.quote('$url'));"
+}
+
+# Decode a string that may have been URL encoded
+function urldecode() {
+
+    local string=$1
+
+    if [[ $string == "" ]]; then
+        echo "A string is needed to decode"
+        return
+    fi
+
+    python -c "import urllib.parse as u; print(u.unquote('$string'));"
+}

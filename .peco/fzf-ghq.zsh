91e158182bfcb9385233e2531d216623f2442afa
togatoga <togasakitogatoga@gmail.com>
2019-02-04 19:10:46 +0900

    Update

--- .peco/fzf-ghq.zsh
+++ .peco/fzf-ghq.zsh
@@ -1,6 +1,6 @@
 # Change directory to the repo where fetched by peco
 function fzf-ghq() {
-	local selected_dir=$(ghq list --full-path | fzf --query "$LBUFFER")
+	local selected_dir=$(ghq list --full-path | fzf --exact --query "$LBUFFER")
 	if [ -n "$selected_dir" ]; then
 		BUFFER="cd ${selected_dir}"
 		zle accept-line

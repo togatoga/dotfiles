99d1d4a5c421586d8f0733d4ac51938080650ba1
togatoga <togasakitogatoga@gmail.com>
2021-05-13 16:54:18 +0900

    Update procon.zsh

--- .zsh/helper/procon.zsh
+++ .zsh/helper/procon.zsh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
 function pro() {
-	dir=$(cpm list | peco --prompt '[dir]')
+	dir=$(cpm list --all | peco --prompt '[dir]')
 	if [ -n ${dir} ]; then
-		echo "cd ${dir}"
-		cd ${dir}
+		num=$(echo ${dir} | awk '{print NF}')
+		if [ ${num} -eq 1 ]; then
+		procon_dir=$(echo ${dir} | awk '{print $0}')
+		else
+		procon_dir=$(echo ${dir} | awk '{print $NF}')
+		fi
+		cd $procon_dir
 		return 1
 	fi
 	return 0
-
 }

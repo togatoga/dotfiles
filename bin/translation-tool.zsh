aa29517e16cd04cd679eaf56dca21c577dbda3d6
togatoga <togasakitogatoga@gmail.com>
2022-11-16 12:54:28 +0900

    Delete vn command

--- bin/translation-tool.zsh
+++ bin/translation-tool.zsh
@@ -1,20 +1,4 @@
 #!/bin/bash
-# git clone https://github.com/togatoga/vn
-# cd vn; "docker build -t vn ."
-
-function docker-build-vn() {
-	pushd
-	rm -rf "/tmp/vn"
-	git clone https://github.com/togatoga/vn /tmp/vn
-	cd /tmp/vn
-	git pull origin master
-	docker build --no-cache=true -t vn .
-	popd
-}
-alias vn="docker run -it --rm vn vn"
-alias vni="docker run -it --rm vn vn translate --interactive"
-alias vnt="docker run -it --rm vn vn translate $*"
-
 # https://github.com/soimort/translate-shell
 function translate() {
 	words=$@

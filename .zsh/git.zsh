de6c9f067afbda19813bf7d6c0ce458a637ef74b
togatoga <hitoshitogasaki@gmail.com>
2023-10-01 22:30:18 +0900

    Update .zsh/git.zsh

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -4,6 +4,6 @@ alias ga="git commit -am"
 alias co="git checkout"
 
 function private() {
-	git config --local user.email togasakitogatoga@gmail.com
+	git config --local user.email hitoshitogasaki@gmail.com
 	git config --local user.name "togatoga"
 }
